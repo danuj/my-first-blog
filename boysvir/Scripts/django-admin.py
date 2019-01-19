9d6c20cb6865badd63de93c5a8ab244ba699305a
danuj <danuj9@gmail.com>
2019-01-19 22:15:59 +0530

    My Django boys app, first commit

---
+++ boysvir/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\a264569\desktop\web_django_learn\djangoboys\boysvir\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
